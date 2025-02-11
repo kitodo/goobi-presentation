slub/kitodo-presentation

Make loaded document a global variable
This will allow the navigation plugin to access the document, e.g., to
get the number of pages.

Classes/Controller/PageViewController.php
@@ -169,7 +169,10 @@ class PageViewController extends AbstractController
             'forceAbsoluteUrl' => !empty($this->settings['forceAbsoluteUrl']),
             'useInternalProxy' => !empty($this->settings['useInternalProxy']),
         ];
+        // TODO: Rethink global tx_dlf_loaded_document
         $viewerConfiguration = '$(document).ready(function() {
+                tx_dlf_loaded_document = ' . json_encode($this->document->getDoc()->toArray($this->uriBuilder, $config)) . ';
+
                 if (dlfUtils.exists(dlfViewer)) {
                     tx_dlf_viewer = new dlfViewer({
                         controls: ["' . implode('", "', $this->controls) . '"],
@@ -178,7 +181,6 @@ class PageViewController extends AbstractController
                         fulltexts: ' . json_encode($this->fulltexts) . ',
                         annotationContainers: ' . json_encode($this->annotationContainers) . ',
                         useInternalProxy: ' . ($this->settings['useInternalProxy'] ? 1 : 0) . ',
-                        document: ' . json_encode($this->document->getDoc()->toArray($this->uriBuilder, $config)) . '
                     });
                 }
             });';

kitodo/goobi-presentation

Classes/Controller/PageViewController.php
@@ -509,7 +509,6 @@ class PageViewController extends AbstractController
                         'div' => "tx-dfgviewer-map-' . $i . '",
                         'progressElementId' => $this->settings['progressElementId'],
                         'counter' => $i,
-                        'document' => $this->document->getCurrentDocument()->toArray($this->uriBuilder, $config),
                         'images' => $docImage,
                         'fulltexts' => $docFulltext,
                         'score' => $docScore,
@@ -528,6 +527,8 @@ class PageViewController extends AbstractController
 
             // Viewer configuration.
             $viewerConfiguration = '$(document).ready(function() {
+                    tx_dlf_loaded_document = ' . json_encode($this->document->getCurrentDocument()->toArray($this->uriBuilder, $config)) . ';
+
                     if (dlfUtils.exists(dlfViewer)) {
                         ' . $jsViewer . '
                         viewerCount = ' . ($i - 1) . ';
@@ -545,12 +546,8 @@ class PageViewController extends AbstractController
             $viewer = [
                 'controls' => $this->controls,
                 'div' => $this->settings['elementId'],
-<<<<<<< HEAD
                 'progressElementId' => $this->settings['progressElementId'] ?? 'tx-dlf-page-progress',
-=======
-                'progressElementId' => $this->settings['progressElementId'],
                 'document' => $this->document->getCurrentDocument()->toArray($this->uriBuilder, $config),
->>>>>>> 3043018a (Add Basic JSON Representation for Document and First Navigation Button)
                 'images' => $this->images,
                 'fulltexts' => $this->fulltexts,
                 'score' => $this->scores,
@@ -564,6 +561,8 @@ class PageViewController extends AbstractController
 
             // Viewer configuration.
             $viewerConfiguration = '$(document).ready(function() {
+                    tx_dlf_loaded_document = ' . json_encode($this->document->getCurrentDocument()->toArray($this->uriBuilder, $config)) . ';
+
                     if (dlfUtils.exists(dlfViewer)) {
                         tx_dlf_viewer = new dlfViewer(' . json_encode($viewer) . ');
                     }